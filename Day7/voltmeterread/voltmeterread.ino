8a566eb90ba38707ab7e606f49e60d37f7c17094
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-15 15:16:21 +0600

    Voltmeter with arduino

---
+++ Day7/voltmeterread/voltmeterread.ino
@@ -0,0 +1,14 @@
+int RE;
+float volt;
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+}
+
+void loop() {
+
+   RE = analogRead(A0);
+   volt=30*RE/1024.0;
+   Serial.println(volt);
+   delay(200);
+}
