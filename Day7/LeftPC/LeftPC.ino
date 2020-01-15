29fc66c11c3d799069efe4816dc58b6d91235a5b
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-15 15:11:16 +0600

    Data transfer between 2 arduino

---
+++ Day7/LeftPC/LeftPC.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial3.begin(115200);
+
+}
+
+void loop() {
+ Serial3.write("1");
+ delay(1000);
+
+}
---
+++ Day7/RightPC/RightPC.ino
@@ -0,0 +1,14 @@
+char x;
+void setup() {
+  Serial.begin(115200);
+  Serial3.begin(115200);
+
+}
+
+void loop() {
+  if(Serial3.available()>0)
+ {
+  char x=Serial3.read();
+  Serial.print(x);
+  }
+} 
