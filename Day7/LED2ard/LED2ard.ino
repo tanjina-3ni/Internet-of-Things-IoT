ad282c3dafbaf412be12d4a7a1649bb982e7fea1
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-15 15:15:24 +0600

    LED on when transmit a character

---
+++ Day7/LED2ard/LED2ard.ino
@@ -0,0 +1,25 @@
+
+int pin=6;
+
+
+void setup() {
+  Serial3.begin(115200);
+  Serial.begin(115200);
+  pinMode(pin,INPUT_PULLUP);
+ 
+}
+
+void loop() {
+
+ char x = digitalRead(pin);
+ if(x==HIGH)
+ {
+  Serial3.print("A");
+ }
+ else
+ {
+   Serial3.print("a");
+ }
+ 
+
+}
---
+++ Day7/LED2ardLpc/LED2ardLpc.ino
@@ -0,0 +1,24 @@
+char x;
+int led=13;
+void setup() {
+  Serial.begin(115200);
+  Serial3.begin(115200);
+  pinMode(led,HIGH);
+
+}
+
+void loop() {
+  if(Serial.available()>0){
+        char x=Serial3.read();
+        if(x=="A")
+           { 
+            //Serial.println(x);
+            digitalWrite(led,HIGH);
+            }
+        else
+        {
+           digitalWrite(led,LOW);
+        }
+  
+} 
+}
