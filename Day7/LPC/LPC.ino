3a520ccfd17b2f0691a94dcc49ef0d0783eef8de
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-15 15:12:59 +0600

    Data transfer between 2 arduino (fullduplex)

---
+++ Day7/LPC/LPC.ino
@@ -0,0 +1,19 @@
+char x;
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200);
+
+}
+
+void loop() {
+  if(Serial.available()>0)
+ {
+  char x=Serial.read();
+  Serial3.println(x);
+  }
+  if(Serial3.available()>0)
+ {
+  char x=Serial3.read();
+  Serial.println(x);
+  }
+} 
---
+++ Day7/RPC/RPC.ino
@@ -0,0 +1,19 @@
+char x;
+void setup() {
+  Serial.begin(115200);
+  Serial3.begin(115200);
+
+}
+
+void loop() {
+  if(Serial.available()>0)
+ {
+  char x=Serial.read();
+  Serial3.print(x);
+  }
+  if(Serial3.available()>0)
+ {
+  char x=Serial3.read();
+  Serial.print(x);
+  }
+} 
