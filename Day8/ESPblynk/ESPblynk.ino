f4ad53a46c222b833395d6fd083f352d13a1ba31
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-16 15:04:05 +0600

    Voltage reading on Blynk

---
+++ Day8/ESPblynk/ESPblynk.ino
@@ -0,0 +1,34 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+
+char auth[] = "3qJOXCMYUujeViyIaYG3kWJKxoVs9G_7";
+char ssid[] = "RUET--Employee";
+char pass[] = "RUETEM#789";
+String x;
+void setup()
+{
+
+   Serial.begin(115200);
+   Serial2.begin(115200);
+
+
+   Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  if(Serial2.available()>0)
+ {   
+  String x= Serial2.readString();
+  Blynk.virtualWrite(V7,x);
+   delay(2000); 
+  }
+                                     
+}
+
