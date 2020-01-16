44544d98f17eb1015d17f270d391dc1281907b4c
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-16 15:02:54 +0600

    voltmeter with ESP 32

---
+++ Day8/LpcwithESP/LpcwithESP.ino
@@ -0,0 +1,19 @@
+int RE;
+float volt; 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial3.begin(115200);
+  pinMode(A0,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+ RE = analogRead(A0);  
+ float volt=30*RE/1024.0;
+ Serial.println(volt);
+ Serial3.println(volt);
+ delay(1000);
+}
---
+++ Day8/rpcwithESP/rpcwithESP.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(115200);
+ 
+
+}
+
+void loop() {
+  if(Serial.available()>0)
+ {  
+  Serial.print(char(Serial.read()));
+  delay(200);
+  }
+} 
