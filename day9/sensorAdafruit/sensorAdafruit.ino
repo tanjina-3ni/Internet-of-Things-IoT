8b972e31a784fce0a63ee89b7614e8ef76211121
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-17 19:53:11 +0600

    sensor data read on serial monitor in adafruit

---
+++ day9/sensorAdafruit/sensorAdafruit.ino
@@ -0,0 +1,125 @@
+/***************************************************
+  Adafruit MQTT Library ESP8266 Example
+ ****************************************************/
+#include <WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// the on off button feed turns this LED on/off
+#define LED 26  
+
+#define PWMOUT 12
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "RUET--Employee"
+#define WLAN_PASS       "RUETEM#789"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "tanjina_3ni"
+#define AIO_KEY         "aio_CVnd48QkPoCgv1duD3q0MChFVi8a"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+Adafruit_MQTT_Publish ap = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/myfruit2");
+
+
+/*************************** Sketch Code ************************************/
+int y;
+int trig=12;
+int echo=13;
+float Tt,s,v=0.034,t;
+void MQTT_connect();
+
+void setup() {
+  pinMode(LED, OUTPUT);
+ 
+
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+
+  Serial.begin(9600);
+  
+}
+
+uint32_t x=0;
+
+void loop() {
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+  Tt=pulseIn(echo,HIGH);
+  t=(float)Tt/2;
+  s=v*t;
+ 
+  MQTT_connect();
+
+ ap.publish(s);
+
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+
+  delay(1000);
+
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
