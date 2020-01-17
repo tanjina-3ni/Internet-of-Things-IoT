73c71a3ebf558c382b025837a336b93e91e180bd
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-17 19:49:28 +0600

    Sensor

---
+++ Day6/Sensor/Sensor/Sensor.ino
@@ -0,0 +1,24 @@
+
+int trig=12;
+int echo=13;
+float Tt,s,v=0.034,t;
+
+void setup() {
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+  Tt=pulseIn(echo,HIGH);
+  t=(float)Tt/2;
+  s=v*t;
+  
+}
